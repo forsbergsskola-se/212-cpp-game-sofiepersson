868c394a790d7451e7a87c917b66c29d679e366d
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2023-04-16 01:51:34 +0200

    Updates GameObject Class

---
+++ Game/Game/GameObject.cpp
@@ -0,0 +1,13 @@
+#include "GameObject.h"
+#include "Window.h"
+#include "Image.h"
+
+GameObject::GameObject(const char* imagePath, Window* window)
+	: image{ window->loadImage(imagePath) }
+{
+
+}
+
+void GameObject::render(Window* window) {
+	window->render(image.get());
+}
--- Game/Game/GameObject.h
+++ Game/Game/GameObject.h
@@ -1,6 +1,19 @@
 #pragma once
+
+#include <memory>
+#include "Image.h"
+
+extern class Window;
+
+using namespace std;
+
 class GameObject
 {
+protected:
+	unique_ptr<Image> image;
+public:
+	GameObject(const char* imagePath, Window* window);
 	virtual void update() = 0;
+	void render(Window* window);
 };
 
