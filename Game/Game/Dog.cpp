32d0610033ab2f854114e74f453a59f4f184e60d
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2023-04-16 14:31:35 +0200

    Updates Dog so Clicking on it now gives Hearts

--- Game/Game/Dog.cpp
+++ Game/Game/Dog.cpp
@@ -1,5 +1,15 @@
 #include "Dog.h"
+#include <iostream>
+
+using namespace std;
 
 void Dog::update() {
+	cout << heartCount << endl;
+}
 
+void Dog::handleEvent(SDL_Event* e) {
+	Button::handleEvent(e);
+	if (inside ) {
+		heartCount += heartsOnClick;
+	}
 }
--- Game/Game/Dog.h
+++ Game/Game/Dog.h
@@ -1,16 +1,19 @@
 #pragma once
+#include "Button.h"
 #include "GameObject.h"
-class Dog :
-    public GameObject
-{
-    int width{ 300 };
-    int height{ 300 };
 
+class Dog : public Button
+{
+    int width{ 200 };
+    int height{ 350 };
+    unsigned int heartCount{};
+    unsigned int heartsOnClick{ 1 };
 public:
-    Dog(Window* window) : GameObject("img/Ludde1.png", window) {
+    Dog(Window* window) : Button("img/Ludde1.png", window) {
         image->width = width;
         image->height = height;
     }
     void update() override;
+    void handleEvent(SDL_Event* e) override;
 };
 
