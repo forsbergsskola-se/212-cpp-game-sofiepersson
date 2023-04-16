c6beb2af947f465822c09d1adbef3d1af193c853
Sofie Persson <sofie.persson@student.forsbergsskola.se>
2023-04-16 14:39:25 +0200

    Changes Dog Position

--- Game/Game/Dog.h
+++ Game/Game/Dog.h
@@ -6,12 +6,15 @@ class Dog : public Button
 {
     int width{ 200 };
     int height{ 350 };
+    int x{ 25 }, y{ 40 };
     unsigned int heartCount{};
     unsigned int heartsOnClick{ 1 };
 public:
     Dog(Window* window) : Button("img/Ludde1.png", window) {
         image->width = width;
         image->height = height;
+        image->x = x;
+        image->y = y;
     }
     void update() override;
     void handleEvent(SDL_Event* e) override;
